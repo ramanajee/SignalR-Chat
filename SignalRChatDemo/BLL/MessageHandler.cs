59c279cae71a3b24cc4bdab4c10bd56562d8091f
Ramanajee Kandregula <ramanajee516@live.in>
2015-11-26 11:01:05 +0530

    SignalR Chat

---
+++ SignalRChatDemo/BLL/MessageHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SignalRChatDemo.BLL
+{
+    [HubName("Message")]
+    public class MessageHandler:Hub
+    {
+       static List<string> clients = new List<string>(); 
+        public void SendMessage(string clientid,string userName,string message)
+        {
+            if (!clients.Contains(clientid))
+            {
+                clients.Add(clientid);
+            }
+            Clients.Clients(clients).send(userName, message, DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss.fff", CultureInfo.InvariantCulture));
+        }
+    }
+}
